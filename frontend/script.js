bb6a39b487316c72a7e3cd7f40078845b30c610e
Kinga Kohari <kinguca@yahoo.com>
2022-02-03 17:45:18 +0100

    Header menu added via JS

---
+++ frontend/script.js
@@ -0,0 +1,27 @@
+function LoadEvent(eventObject){
+    //console.log(eventObject);
+    const rootElement = document.getElementById("root");
+    //console.dir(document);
+    console.dir(rootElement);
+
+    const listOfSectionElements = document.querySelectorAll("section")
+    console.log(listOfSectionElements);
+
+    //rootElement.classList.add("newClass");
+    // listOfSectionElements[0].classList.add("newClass")
+    // listOfSectionElements[1].classList.add("newClass")
+    // listOfSectionElements[2].classList.add("newClass")
+    // listOfSectionElements[3].classList.add("newClass")
+    // listOfSectionElements[4].classList.add("newClass")
+
+let anchors = "";
+
+for (const sectionElement of listOfSectionElements) {
+    //sectionElement.classList.add("newClass");
+    //anchors = anchors + `<a> ${sectionElement.id} </a>`
+    anchors += `<a href="#${sectionElement.id}"> ${sectionElement.id} </a>`;
+}
+    console.log(anchors);
+    rootElement.insertAdjacentHTML("afterbegin",`<header>${anchors}</header>`) //header előjön a weboldalon
+}
+window.addEventListener("load", LoadEvent);
